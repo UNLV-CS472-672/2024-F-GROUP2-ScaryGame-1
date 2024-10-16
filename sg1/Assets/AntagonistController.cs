f833698ee616049d4f6e6190b6abdd6b659275bf
uid106 <peytonnoel@gmail.com>
2024-10-15 20:39:28 -0700

    Introduction of Antagonist with Basic Movement Logic

---
+++ sg1/Assets/AntagonistController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class AntagonistController : MonoBehaviour
+{
+    // Maximum Speed
+    public float speed = 3.0f;
+    // Maximum Velocity
+    public float maxVelocity = 5.0f;  
+    // Distance away from player antagonist should maintain
+    public float stopDistance = 2.0f; 
+    // Target of Antagonist
+    public Transform target;  
+    // Rigid Body
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // Get the Rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            // Finding Distance away from Player
+            float distance = Vector3.Distance(transform.position, target.position);
+
+            // If larger than defined stopping Distance, move closer
+            if (distance > stopDistance)
+            {
+                // Direction and Force
+                Vector3 direction = (target.position - transform.position).normalized;
+                rb.AddForce(direction * speed);
+
+                // Clamp velocity so antagonist doesn't bounce back and forth
+                rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxVelocity);
+            }
+            else
+            {
+                // Stops Antagonist so he doesn't continually run at full speed
+                rb.linearVelocity = Vector3.zero;
+            }
+        }
+    }
+}
