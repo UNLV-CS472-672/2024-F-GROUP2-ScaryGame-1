326c46e68b681030361a14c52d948b4702ae3703
krain0 <51110702+krain0@users.noreply.github.com>
2024-10-27 19:05:31 -0700

    Restructured files so they can be used in tests more easily
    Moved all monobehavior scripts to Scripts/GameLogic.
    
    Also added Scripts/Interfaces for any intefaces that we end up using. Having intefaces makes it easier to make mocks.

--- sg1/Assets/Scripts/GameLogic/DoorController.cs
+++ sg1/Assets/Scripts/GameLogic/DoorController.cs
@@ -4,28 +4,28 @@ public class DoorController : MonoBehaviour
 {
 
     // The Animator component manages the door opening and closing animations.
-    private Animator animator;
+    private IAnimator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        animator = new AnimatorWrapper(GetComponentInChildren<Animator>());
     }
 
     // These functions just return the current state of the animation.
     // The animator component uses a state machine to keep track of the door being 
     // open/closed or currently opening/closing. 
     bool inOpenState() {
-        return animator.GetCurrentAnimatorStateInfo(0).IsName("open");
+        return animator.CompareAnimatorStateName("open");
     }
     bool inClosedState() {
-        return animator.GetCurrentAnimatorStateInfo(0).IsName("closed");
+        return animator.CompareAnimatorStateName("closed");
     }
     bool inOpeningState() {
-        return animator.GetCurrentAnimatorStateInfo(0).IsName("opening");
+        return animator.CompareAnimatorStateName("opening");
     }
     bool inClosingState() {
-        return animator.GetCurrentAnimatorStateInfo(0).IsName("closing");
+        return animator.CompareAnimatorStateName("closing");
     }
 
     // Opens the door if it is currently closed or closes the door if it is currently open.
---
+++ sg1/Assets/Scripts/Interfaces/IAnimator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using static Codice.Client.Common.WebApi.WebApiEndpoints;
+
+public interface IAnimator
+{
+    bool CompareAnimatorStateName(string stateName);
+    bool GetBool(string boolName);
+    void SetBool(string boolName, bool value);
+}
+
+public class AnimatorWrapper : IAnimator
+{
+    Animator animator;
+
+    public AnimatorWrapper(Animator animator)
+    {
+        this.animator = animator;
+    }
+    public bool CompareAnimatorStateName(string stateName)
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    public bool GetBool(string boolName)
+    {
+        return animator.GetBool(boolName);
+    }
+
+    public void SetBool(string boolName, bool value)
+    {
+        animator.SetBool(boolName, value);
+    }
+}
