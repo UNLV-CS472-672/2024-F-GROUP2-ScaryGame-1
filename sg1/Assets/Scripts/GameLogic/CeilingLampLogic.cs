1bdf1716d6f193c4d41ba29276e5452a9872106d
caseycodes32 <141337193+caseycodes32@users.noreply.github.com>
2024-11-18 15:50:30 -0800

    Updated CeilingLampLogic.cs to include random flickering
    - Added a random event for each individual light to flicker. This flicker is a once-off-once-on action, and it is visually different from the flicker that occurs when the antagonist nears a lamp.

--- sg1/Assets/Scripts/GameLogic/CeilingLampLogic.cs
+++ sg1/Assets/Scripts/GameLogic/CeilingLampLogic.cs
@@ -6,26 +6,34 @@ public class CeilingLampLogic : MonoBehaviour
 {
     public Transform AntagonistTransform;
 
+    ///// KILL SWITCH v /////
+    private const bool ENABLED = true;
+
     private const float LIGHTOFFRADIUS = 1.6f;
     private const float OFFTIME = 2.0f;
     private const float INTENSITY = 0.7f;
-    private const bool ENABLED = true;
+    private const int FLICKERWAITLOW = 10, FLICKERWAITHIGH = 50;
 
     private float gametime_at_off = 0f;
     private bool light_on = true;
+    private int flicker_wait = 0;
+    private bool waiting_to_flicker = false;
     private Light ThisLight;
     private MeshRenderer ThisRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Allows for all lights to be turned off with one line of code. See ENABLED const
+        if (!ENABLED) this.enabled = false;
+
         // Initialize light object
         ThisLight = this.GetComponent<Light>();
         // Initialize renderer object
         ThisRenderer = this.GetComponent<MeshRenderer>();
         // Sets light intensity based on variable
         ThisLight.intensity = INTENSITY;
-
-        if (!ENABLED) this.enabled = false;
+        //Initialize random flicker wait
+        flicker_wait = Random.Range(FLICKERWAITLOW, FLICKERWAITHIGH);
 
     }
 
@@ -39,6 +47,11 @@ public class CeilingLampLogic : MonoBehaviour
             if (Vector3.Distance(this.gameObject.transform.position, AntagonistTransform.position) < LIGHTOFFRADIUS)
             {
                 LightFlickerOff();
+            }
+            else
+            {
+                RandomLightFlicker();
+
             }
         }
         else
@@ -74,7 +87,6 @@ public class CeilingLampLogic : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         ThisLight.enabled = false;
         ThisRenderer.materials[1].SetVector("_EmissionColor", new Vector4(0.1f, 0.1f, 0.1f));
-
     }
 
     // A function that is called to turn the light object off
@@ -94,6 +106,34 @@ public class CeilingLampLogic : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         ThisLight.enabled = true;
         ThisRenderer.materials[1].SetVector("_EmissionColor", new Vector4(0.5f, 0.5f, 0.5f));
+    }
 
+    // A function that is called to turn the light object off and on quickly
+    private void RandomLightFlicker()
+    {
+        // Ensure that we aren't already waiting for the lightbulb to flicker off and on
+        if (waiting_to_flicker == false)
+        {
+            // Start thread to wait for a flicker event
+            waiting_to_flicker = true;
+            StartCoroutine(FlickerTimedActions());
+        }
+    }
+    // A time-based, blocking function that turns the light object of the lamp off then on
+    private IEnumerator FlickerTimedActions()
+    {
+        // Wait for predetermined random amount of time before flickering
+        yield return new WaitForSeconds(flicker_wait);
+        // Turn light off, modify material, wait 0.1s, turn light back on
+        ThisLight.enabled = false;
+        ThisRenderer.materials[1].SetVector("_EmissionColor", new Vector4(0.1f, 0.1f, 0.1f));
+        yield return new WaitForSeconds(0.1f);
+        ThisLight.enabled = true;
+        ThisRenderer.materials[1].SetVector("_EmissionColor", new Vector4(0.5f, 0.5f, 0.5f));
+
+        // Re-enable flicker countdown
+        waiting_to_flicker = false;
+        // Generate a new random amount of time to wait
+        flicker_wait = Random.Range(FLICKERWAITLOW, FLICKERWAITHIGH);
     }
 }
