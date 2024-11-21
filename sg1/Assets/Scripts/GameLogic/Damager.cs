772e0eef9529cabb20134812eaf5cc347a0c7cb9
krain0 <51110702+krain0@users.noreply.github.com>
2024-11-21 02:07:32 -0800

    Added cooldown for damaging player

--- sg1/Assets/Scripts/GameLogic/Damager.cs
+++ sg1/Assets/Scripts/GameLogic/Damager.cs
@@ -7,7 +7,11 @@ public class Damager : MonoBehaviour
     public float nextFrameDamage = 0.5f;
     public Transform playerTransform;
     public HealthSlider healthSlider;
-    private bool damagedLastFrame = false;
+    private bool isDamaging = false;
+    private float damagingTimer = 0f;
+    private float damagingTimeLimit = 2f;
+    private float cooldownTimer = 2f;
+    private float cooldownTimeLimit = 2f;
 
     void Start()
     {
@@ -18,24 +22,32 @@ public class Damager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Once the antagonist starts to damage the player, they can continue damaging them for $damagingTimeLimit seconds
+        // then it has to wait for $coolDownTimeLimit seconds before damaging again
+        if(isDamaging) damagingTimer += Time.deltaTime;
+        cooldownTimer += Time.deltaTime;
+
+        if(damagingTimer > damagingTimeLimit)
+        {
+            cooldownTimer = 0f;
+            damagingTimer = 0f;
+            isDamaging = false;
+        }
+
         // Because of the autostopping distance on the NavMeshAgent, I am just doing a distance check, not collision
         // Could probably be improved
-        if(Vector3.Distance(transform.position, playerTransform.position) < 1.0f)
+        if (cooldownTimer > cooldownTimeLimit && Vector3.Distance(transform.position, playerTransform.position) < 1.0f)
         {
-            // Do more damage on first frame of contact
-            if (!damagedLastFrame)
+            if(!isDamaging)
             {
+                damagingTimer = 0f;
                 FirstContactDamage();
             } else
             {
                 ContinuedDamage();
             }
-
-            damagedLastFrame = true;
-        } else
-        {
-            damagedLastFrame = false;
-        }
+            isDamaging = true;
+        } 
 
         // If player runs out of health, load the title scene
         if(healthSlider.currentHealth <= 0)
