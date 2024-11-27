49660cd88b4a5861fa003e073a19675bb76b1ddd
caseycodes32 <141337193+caseycodes32@users.noreply.github.com>
2024-11-26 21:25:52 -0800

    Testing ways to draw points to screen

--- sg1/Assets/MapController.cs
+++ sg1/Assets/MapController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
+
+    private RectTransform rect_transform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rect_transform = this.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -13,4 +15,12 @@ public class MapController : MonoBehaviour
     {
         
     }
+
+    void OnDrawGizmos()
+   {
+       Rect rect = new Rect(10, 10, 150, 100);
+       UnityEditor.Handles.BeginGUI();
+       UnityEditor.Handles.DrawSolidRectangleWithOutline(rect, Color.red, Color.white);
+       UnityEditor.Handles.EndGUI();
+   }
 }
