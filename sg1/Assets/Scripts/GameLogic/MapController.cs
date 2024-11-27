1b0ecba32c31b81719bce2ffe3a98f41a444a5ac
caseycodes32 <141337193+caseycodes32@users.noreply.github.com>
2024-11-27 01:10:00 -0800

    Updated MapController.cs to only work with keypress
    - Added code that checks for a key [M] being pressed to display the map and run the calculations to translate positions to display coordinates

--- sg1/Assets/Scripts/GameLogic/MapController.cs
+++ sg1/Assets/Scripts/GameLogic/MapController.cs
@@ -10,20 +10,12 @@ public class MapController : MonoBehaviour
     public GameObject AntagonistUIDot;
     public Transform AntagonistTransform;
 
-    public GameObject Minigame1;
-    public GameObject Minigame2;
-    public GameObject Minigame3;
-    public GameObject Minigame4;
-    public GameObject Minigame5;
-
     private RectTransform rt_player, rt_antagonist;
 
-    private float map_bounds_z1 = -6.2f;
-    private float map_bounds_z2 = 23.5f;
     private float map_bounds_x1 = -15.6f;
     private float map_bounds_x2 = 14.3f;
-    private float map_bound_z_mean = 0f;
-    private float map_bound_x_mean = 0f;
+    private float map_bounds_z1 = -6.2f;
+    private float map_bounds_z2 = 23.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,28 +23,21 @@ public class MapController : MonoBehaviour
         rt_player = PlayerUIDot.GetComponent<RectTransform>();
         rt_antagonist = AntagonistUIDot.GetComponent<RectTransform>();
 
-        
-
-        //map_bound_x_mean = (map_bounds_x1 + map_bounds_x2 / 2f);
-        //map_bound_z_mean = (map_bounds_z1 + map_bounds_z2 / 2f);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (PlayerTransform.position.x > map_bound_x_mean)
+        if (Input.GetKey(KeyCode.M))
         {
-            rt_player.localPosition = new Vector3(((PlayerTransform.position.x / map_bounds_x2) * 170f), 0f, 0f);
+            UpdateGameobjectActive(true);
+            UpdateMinimapDotPosition(rt_player, PlayerTransform.transform.position);
+            UpdateMinimapDotPosition(rt_antagonist, AntagonistTransform.transform.position);
         }
         else
         {
-            rt_player.localPosition = new Vector3(((PlayerTransform.position.x / map_bounds_x1) * -170f), 0f, 0f);
+            UpdateGameobjectActive(false);
         }
-        */
-        UpdateMinimapDotPosition(rt_player, PlayerTransform.transform.position);
-        UpdateMinimapDotPosition(rt_antagonist, AntagonistTransform.transform.position);
 
     }
 
@@ -60,5 +45,13 @@ public class MapController : MonoBehaviour
     {
         RT_Dot.localPosition = new Vector3(((((WorldPosition.x + Mathf.Abs(map_bounds_x1)) / (Mathf.Abs(map_bounds_x1) + Mathf.Abs(map_bounds_x2))) * 340f) - 170f), 0f, 0f);
         RT_Dot.localPosition = new Vector3(RT_Dot.localPosition.x, ((((WorldPosition.z + Mathf.Abs(map_bounds_z1)) / (Mathf.Abs(map_bounds_z1) + Mathf.Abs(map_bounds_z2))) * 340f) - 170f), 0f);
+    }
+
+    void UpdateGameobjectActive(bool active)
+    {
+        MapBackground.SetActive(active);
+        Map.SetActive(active);
+        PlayerUIDot.SetActive(active);
+        AntagonistUIDot.SetActive(active);
     }
 }
