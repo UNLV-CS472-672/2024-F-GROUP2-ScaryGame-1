75684b1837d2ff49b917016bce28566ee17f35f0
Brandon <37670357+brandon722@users.noreply.github.com>
2024-11-29 17:01:04 -0800

    Changed default master volume to 100.

--- sg1/Assets/Scripts/Sound/AudioSettingsManager.cs
+++ sg1/Assets/Scripts/Sound/AudioSettingsManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class AudioSettingsManager
 {
-    private static float _masterVolume = 0.5f; // Default volume
+    private static float _masterVolume = 1.0f; // Default volume
 
     public static float MasterVolume
     {
